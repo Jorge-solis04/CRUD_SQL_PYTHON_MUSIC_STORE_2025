86ef430deb926d7bf3dae8e61d81aae81867fe25
Jorge-solis04 <142189789+Jorge-solis04@users.noreply.github.com>
2025-05-25 07:18:41 -0600

    [ADDED]: Se agrego el login totalmente funcional, ademas de unos cambios en la sintaxis debido a la modificacion de la base de datos

---
+++ db.py
@@ -0,0 +1,12 @@
+import pymysql
+import config
+
+def get_connection():
+    return pymysql.connect(
+        host=config.MYSQL_HOST,
+        user=config.MYSQL_USER,
+        password=config.MYSQL_PASSWORD,
+        db=config.MYSQL_DB,
+        port=config.MYSQL_PORT,
+        cursorclass=pymysql.cursors.DictCursor
+    )
